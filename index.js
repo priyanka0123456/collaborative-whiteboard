519278050e97c728a9f93af8ecebeff531babaeb
priyanka <priyankarar4595@gmail.com>
2025-03-26 19:27:54 +0530

    Initial commit for WebSocket server

---
+++ index.js
@@ -0,0 +1,29 @@
+let express = require("express");
+let app = express();
+let httpServer = require("http").createServer(app);
+let io = require("socket.io")(httpServer);
+
+let connections = [];
+
+io.on("connect", (socket) => {
+  connections.push(socket);
+  console.log(`${socket.id} has connected`);
+
+  socket.on("propogate", (data) => {
+    connections.map((con) => {
+      if (con.id !== socket.id) {
+        con.emit("onpropogate", data);
+      }
+    });
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(`${socket.id} is disconnected`);
+    connections = connections.filter((con) => con.id !== socket.id);
+  });
+});
+
+app.use(express.static("public"));
+
+let PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => console.log(`Server started on port ${PORT}`));
